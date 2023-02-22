b955966f9a0ef312323b18733fe7d352e7689930
lsenaa <lsenaa3838@gmail.com>
2023-02-22 17:50:37 +0900

    Update Mypage

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,9 +1,9 @@
+import styled from "@emotion/styled";
 import { useState } from "react";
 import { useAuth } from "../../src/components/commons/hooks/useAuth";
 import { UseQueryFetchUserLoggedIn } from "../../src/components/commons/hooks/useQueries/UseQueryFetchUserLoggedIn";
 import MypageMypost from "../../src/components/units/mypage/mypost/Mypost";
 import MypageMyprofile from "../../src/components/units/mypage/myprofile/Myprofile";
-import * as S from "./styles";
 
 export default function MyPage() {
   useAuth();
@@ -19,10 +19,10 @@ export default function MyPage() {
   };
 
   return (
-    <S.Wrapper>
-      <S.Title>MY PAGE</S.Title>
-      <S.UserWrapper>
-        <S.UserPicture
+    <Wrapper>
+      <Title>MY PAGE</Title>
+      <UserWrapper>
+        <UserPicture
           src={
             data?.fetchUserLoggedIn.picture
               ? `https://storage.googleapis.com/${data?.fetchUserLoggedIn.picture}`
@@ -30,15 +30,79 @@ export default function MyPage() {
           }
           alt="사용자 프로필이미지"
         />
-        <S.UserName>{data?.fetchUserLoggedIn.name}</S.UserName>
-        <S.UserEmail>{data?.fetchUserLoggedIn.email}</S.UserEmail>
-      </S.UserWrapper>
-      <S.MyMenuWrapper>
-        <S.MyMenu onClick={onClickMypost}>My Post</S.MyMenu>
-        <S.MyMenu onClick={onClickMyprofile}>My Profile</S.MyMenu>
-      </S.MyMenuWrapper>
+        <UserName>{data?.fetchUserLoggedIn.name}</UserName>
+        <UserEmail>{data?.fetchUserLoggedIn.email}</UserEmail>
+      </UserWrapper>
+      <MyMenuWrapper>
+        <MyMenu onClick={onClickMypost}>My Post</MyMenu>
+        <MyMenu onClick={onClickMyprofile}>My Profile</MyMenu>
+      </MyMenuWrapper>
       {isSelected[0] && <MypageMypost />}
       {isSelected[1] && <MypageMyprofile />}
-    </S.Wrapper>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  width: 1200px;
+  box-shadow: 0px 0px 10px grey;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  color: white;
+  font-size: 32px;
+  font-weight: bold;
+`;
+
+const UserWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 40px 0;
+`;
+
+const UserPicture = styled.img`
+  width: 80px;
+  height: 80px;
+  margin-bottom: 10px;
+  border-radius: 50%;
+`;
+
+const UserName = styled.div`
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const UserEmail = styled.div`
+  color: white;
+  font-size: 20px;
+  margin: 10px 0;
+`;
+
+const MyMenuWrapper = styled.div`
+  width: 100%;
+  padding: 20px 0;
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MyMenu = styled.div`
+  color: white;
+  font-size: 24px;
+  margin-right: 30px;
+  cursor: pointer;
+
+  :hover {
+    color: #f9d142;
+  }
+`;
