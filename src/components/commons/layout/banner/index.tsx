aab1403a1c89fc61195aecdc8b908804d8b39671
lsenaa <lsenaa3838@gmail.com>
2023-02-22 17:14:50 +0900

    Update LayoutBanner

--- src/components/commons/layout/banner/index.tsx
+++ src/components/commons/layout/banner/index.tsx
@@ -1,60 +1,14 @@
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import * as S from "./styles";
+import { ChangeEvent, KeyboardEvent } from "react";
 
-interface NextArrowProps {
-  className?: any;
-  style?: any;
-  onClick?: React.MouseEventHandler<HTMLDivElement>;
+interface ILayoutBannerProps {
+  onChangeQuery: (event: ChangeEvent<HTMLInputElement>) => void;
+  onSubmitSearch: (event: KeyboardEvent<HTMLInputElement>) => void;
 }
 
-const PrevArrow = ({ className, style, onClick }: NextArrowProps) => {
-  return (
-    <div
-      className={className}
-      style={{
-        ...style,
-        display: "block",
-        position: "absolute",
-        left: "20px",
-        zIndex: 9,
-        width: "48px",
-      }}
-      onClick={onClick}
-    />
-  );
-};
-
-const NextArrow = ({ className, style, onClick }: NextArrowProps) => {
-  return (
-    <div
-      className={className}
-      style={{
-        ...style,
-        display: "block",
-        position: "absolute",
-        right: "20px",
-        zIndex: 9,
-      }}
-      onClick={onClick}
-    />
-  );
-};
-
-export default function LayoutBanner(props) {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    prevArrow: <PrevArrow />,
-    nextArrow: <NextArrow />,
-  };
-
+export default function LayoutBanner(props: ILayoutBannerProps) {
   return (
     <S.Wrapper>
       <S.InnerWrapper>
