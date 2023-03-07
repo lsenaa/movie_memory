197c9e31c3a7077836a0fc51f34c21c9a7a03156
lsenaa <lsenaa3838@gmail.com>
2023-03-07 16:40:55 +0900

    Update scripts

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  trailingSlash: true,
+  generateBuildId: () => "movie_memory",
+
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
