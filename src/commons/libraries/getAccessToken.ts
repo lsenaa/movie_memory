6392bdf24afc2076e90befbbc7b09baaff04c557
lsenaa <lsenaa3838@gmail.com>
2023-03-08 16:00:05 +0900

    'Update scripts'

--- src/commons/libraries/getAccessToken.ts
+++ src/commons/libraries/getAccessToken.ts
@@ -15,7 +15,7 @@ export const getAccessToken = async () => {
       { credentials: "include" }
     );
     const result = await graphQLClient.request(RESTORE_ACCESS_TOKEN);
-    const newAccessToken = result.restoreAccessToken.accessToken;
+    const newAccessToken = String(result.restoreAccessToken.accessToken);
 
     return newAccessToken;
   } catch (error) {
